fa88d37bc53473f167a2d2985149286bf88a2e74
theBashShell <edohbruno@gmail.com>
2020-02-12 18:49:25 +0000

    tree recursive exploration

---
+++ recursive.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(data){
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+}
+
+head = new Node(4)
+head.left = new Node(3)
+head.right = new Node(10)
+head.right.right = new Node(11)
+
+console.table(head)
