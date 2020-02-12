120f413a0035f7681a2f580e17da9f7555c6ac17
theBashShell <edohbruno@gmail.com>
2020-02-12 18:59:15 +0000

    inorder tree traversal

--- recursive.js
+++ recursive.js
@@ -12,3 +12,17 @@ head.right = new Node(10)
 head.right.right = new Node(11)
 
 console.table(head)
+
+function indorder(head) {
+  if(head === null) return 
+  // left
+  indorder(head.left)
+  
+  // parent
+  console.log(head.data)
+  
+  // right
+  indorder(head.right)
+}
+
+indorder(head)
