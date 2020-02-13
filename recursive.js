108e9265f1eae0478935890e20ae7b89d8b54f77
theBashShell <edohbruno@gmail.com>
2020-02-13 01:48:19 +0000

    yes postorder traversal iterative implemented

--- recursive.js
+++ recursive.js
@@ -32,6 +32,29 @@ function postorder(head) {
   console.log(head.data);
 }
 
+function postorderIterative(head) {
+  let stack = new Array();
+  stack.push(head);
+
+  while (stack.length !== 0) {
+    let current = stack[stack.length - 1];
+    if (!current.left && !current.right) {
+      console.log(current.data);
+      current = stack.pop();
+    } else {
+      if (current.right) {
+        stack.push(current.right);
+        current.right = null;
+      }
+
+      if (current.left) {
+        stack.push(current.left);
+        current.left = null;
+      }
+    }
+  }
+}
+
 function preorder(head) {
   if (head === null) return;
   console.log(head.data);
@@ -51,10 +74,11 @@ function preorderRecursive(head) {
   }
 }
 
-indorder(head);
-console.log("************");
 postorder(head);
 console.log("************");
-preorder(head);
-console.log("************");
-preorderRecursive(head);
+postorderIterative(head);
+// indorder(head);
+// console.log("************");
+// preorder(head);
+// console.log("************");
+// preorderRecursive(head);
