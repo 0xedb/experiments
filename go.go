71c81b95fb54e9648a323581bc76248cc2e199dc
theBashShell <edohbruno@gmail.com>
2020-02-25 23:21:41 +0000

    functions

--- go.go
+++ go.go
@@ -8,4 +8,10 @@ import (
 func main() {
 	fmt.Println("one")
 	fmt.Println(math.Abs(-239393))
+	fmt.Println(Adder(220, 23))
+}
+
+// Adder adds 2 numbers
+func Adder(one, two int) int {
+	return one + two
 }
