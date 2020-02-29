7843517a0ad946d1b9074529988af1f612feda83
theBashShell <edohbruno@gmail.com>
2020-02-29 23:34:10 +0000

    failed channel

--- go.go
+++ go.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func runnable(ch *chan int) {
+func send(ch chan string, wg *sync.WaitGroup) {
+	wg.Add(1)
+	for {
+		fmt.Println("sent")
+		ch <- "sent"
+	}
+	defer wg.Done()
+}
 
+func receive(ch chan string, wg *sync.WaitGroup) {
+	wg.Add(1)
+	for {
+		r := <-ch
+		fmt.Println(r)
+	}
+	defer wg.Done()
 }
 
 func main() {
-	fmt.Println("hello")
-} 
+	var wg sync.WaitGroup
+	channel := make(chan string)
+	go send(channel, &wg)
+	// go receive(channel, &wg) 
+	defer wg.Wait()
+}
