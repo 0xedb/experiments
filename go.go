dfe1cb4a3e181625918ec8e2b8f292c6300297a6
theBashShell <edohbruno@gmail.com>
2020-03-01 22:27:43 +0000

    hello march

--- go.go
+++ go.go
@@ -2,42 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync"
-) 
-
-func send(ch chan string, wg *sync.WaitGroup) {
-	wg.Add(1)
-	for {
-		fmt.Println("sent")
-		ch <- "sent"
-	} 
-	defer wg.Done() 
-}
-
-func receive(ch chan string, wg *sync.WaitGroup) {
-	wg.Add(1)
-	for {
-		r := <-ch
-		fmt.Println(r)
-	} 
-	defer wg.Done() 
-}
-
-func runnable(wg *sync.WaitGroup) {
-	wg.Add(1)
-	a := 0
-	for {
-		fmt.Printf("%v::\n", a)
-		if a == 20 {
-			wg.Done()
-			return
-		}  
-	}
-
-}
+)
 
 func main() {
-	var wg sync.WaitGroup  
-	go runnable(&wg)
-	wg.Wait()
+	fmt.Println("hello march")
 }
