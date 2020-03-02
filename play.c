fb96d1e92dc125fa7292d8e0eba5e252fff7a1c1
theBashShell <edohbruno@gmail.com>
2020-03-02 21:15:07 +0000

    const & pointer

--- play.c
+++ play.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 
 int double_it(int*);
+int const_next(const int*);
 
 int main(void)
 {  
   int n = 10;
   double_it(&n); 
+  int v = const_next(&n);
 
   printf("%d", n);
+  printf("\n%d\n", v);
 
   return 0;
 }
@@ -17,3 +20,9 @@ int main(void)
 int double_it(int* num) {
   return *num *= 2;
 }
+
+int const_next(const int* num) {
+  return *num + 1;
+}
+
+
