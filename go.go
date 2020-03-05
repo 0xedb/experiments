4be83115f40f7e5156769e51c57dfa759eccc4e4
theBashShell <edohbruno@gmail.com>
2020-03-05 23:37:24 +0000

    grouped declaration

--- go.go
+++ go.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("hello march")
+	var(
+		many = "many"
+		atom = 'a'
+		one = 1
+		two = 2.0
+		falsy = false
+		truthy = true
+	)
+	fmt.Printf("%v\n", many)
+	fmt.Printf("%v\n", atom)
+	fmt.Printf("%v\n", one)
+	fmt.Printf("%v\n", two)
+	fmt.Printf("%v\n", falsy)
+	fmt.Printf("%v\n", truthy) 
 }
