095771465253da040824b2c83ae4e2443f13c932
theBashShell <edohbruno@gmail.com>
2020-03-08 13:16:54 +0000

    need for weakmaps

--- iterable.js
+++ iterable.js
@@ -1,20 +1,20 @@
 let nos = {
   first: 1,
-  last: 6
+  last: 3
 };
 
 let arr = {
-  0: 'one',
-  1: 'two',
-  2: 'three',
+  0: "one",
+  1: "two",
+  2: "three",
   length: 3
-}
+};
 
 nos[Symbol.iterator] = function() {
   return {
-    current: this.first, 
+    current: this.first,
     last: this.last,
-    next()   {
+    next() {
       // console.log(this)
       if (this.current <= this.last)
         return { value: this.current++, done: false };
@@ -24,4 +24,16 @@ nos[Symbol.iterator] = function() {
 };
 
 for (let a of nos) console.log(a);
-console.log(arr[0])
+console.log(arr[2]);
+
+console.log(Array.from(nos));
+
+// need for weakmaps
+let john = {
+  name: "jon"
+};
+
+nos.john = john
+john = null
+console.log(john)
+console.log(nos)
