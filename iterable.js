33b9d1f3ea03a32151a17332e3542d82c51794b8
theBashShell <edohbruno@gmail.com>
2020-03-08 12:21:55 +0000

    iterator implemented

--- iterable.js
+++ iterable.js
@@ -1,18 +1,19 @@
 let nos = {
   first: 1,
-  last: 100
-}; 
+  last: 6
+};
 
 nos[Symbol.iterator] = function() {
-  let current = this.first
-  
   return {
-    next: () => {
+    current: this.first, 
+    last: this.last,
+    next()   {
+      // console.log(this)
       if (this.current <= this.last)
         return { value: this.current++, done: false };
       return { done: true };
     }
   };
 };
- 
+
 for (let a of nos) console.log(a);
