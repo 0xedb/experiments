adb5090971c70e27097cdf914762a95bf3b6d1e9
theBashShell <edohbruno@gmail.com>
2020-03-16 18:06:50 +0000

    singly linked list in c

--- play.c
+++ play.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
+struct Node
+{
+  int *data;
+  struct Node *next;
+};
 
-int main(void) {
-  int16_t age = 232;
-  printf("you are %d year(s) old", age);
+struct SinglyLinkedList
+{
+  struct Node *head;
+};
+
+void insert(struct SinglyLinkedList *, int *);
+void traverse(struct SinglyLinkedList *);
+
+int main(void)
+{ 
+  int a = 10;
+  int b = 30;
+  int c = 44;
+  int d = -70;
+  struct SinglyLinkedList *sll = malloc(sizeof(struct SinglyLinkedList));
+  insert(sll, &a);
+  insert(sll, &b);
+  insert(sll, &c);
+  insert(sll, &d);
+  traverse(sll);
+  free(sll);
   return 0;
+}
+
+void insert(struct SinglyLinkedList *ll, int *val)
+{
+  struct Node *latest = (struct Node *)malloc(sizeof(struct Node));
+  latest->data = val;
+  latest->next = ll->head;
+  ll->head = latest;
+}
+
+void traverse(struct SinglyLinkedList *ll)
+{
+  struct Node *nav = ll->head;
+  while (nav)
+  {
+    printf("%d\n", *nav->data);
+    nav = nav->next;
+  }
 }
