0de36b54054e3e8e9a68895b8036a585c4a74e4d
theBashShell <edohbruno@gmail.com>
2020-03-18 15:03:01 +0000

    more on types

--- go.go
+++ go.go
@@ -3,14 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	type avid int
+	type bvid = bool
 	a := 010
 	ab := 0xA
 	abb := 0b110
 	folder := `C:\n`
 	f := "C:\n"
+	var b avid = 3000;
+	var c bvid = true
 	fmt.Println("hey");
 	fmt.Println(a, ab);
 	fmt.Println(abb)
 	fmt.Println(folder)
 	fmt.Println(f)
+	fmt.Println(b)
+	fmt.Println(c)
 }
