60b47d6ab21901477d118cb960f22128579b66d5
theBashShell <edohbruno@gmail.com>
2020-03-21 22:57:12 +0000

    c++ classes advanced

--- cpp.cpp
+++ cpp.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 
-int mayer(int* a) {
+int mayer(int *a)
+{
   return a[0];
 }
 
-int main(void) {
+class Node
+{
+public:
+  int val;
+  Node* next;
+  // Node(int val) {
+  //   val = val;
+  // }
+  std::string str(void);
+};
+
+std::string Node::str(void) {
+  return "---> " + val;
+}
+
+int main(void)
+{
   int arr[] = {30, 230, 3};
+  Node aa{};
+  aa.val = 4300;
   std::cout << mayer(arr) << std::endl;
+  std::cout << aa.str() << std::endl;
 }
