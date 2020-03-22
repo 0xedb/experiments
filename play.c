c4d38464bd2de37544dab6d43fd5a0dc178a82ee
theBashShell <edohbruno@gmail.com>
2020-03-22 16:56:54 +0000

    signals in c

--- play.c
+++ play.c
@@ -1,49 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <threads.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <signal.h>
 
-struct Node
+void kill_handler(int sig)
 {
-  int *data;
-  struct Node *next;
-} * head;
-
-void insert(struct Node *, int *);
-void traverse(struct Node *);
-
-void writ()
-{
-  int i = 1;
-  while (i <= 10)
-    printf("thrd:\t%d\n", i++);
+  printf("noooo");
+  write(STDOUT_FILENO, "ohhhhhh", 7);
 }
 
 int main(void)
 {
-  // thrd_t thread;
-  // thrd_create(&thread, (thrd_start_t)writ, NULL);
-  // thrd_join(thread, NULL);
-
-  int a = 10;
-  int b = 30;
-  int c = 44;
-  int d = -70;  
-  insert(head, &a);
-  insert(head, &b);
-  insert(head, &c);
-  insert(head, &d);
-  traverse(head); 
-  return 0;
-}
-
-void insert(struct Node *head, int *val)
-{ 
-  struct Node latest = {val, head}; 
-  *head = latest;
-}
-
-void traverse(struct Node *head)
-{
-   
+  // signal(SIGINT, kill_handler);
+  while (true)
+    printf("ID--------->%d\n", getpid());
 }
