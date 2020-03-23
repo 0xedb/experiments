da29907ff7e30dc5dfaa4f47e10bcc9b0a31041c
theBashShell <edohbruno@gmail.com>
2020-03-23 16:58:24 +0000

    go slice literal myth busted

--- go.go
+++ go.go
@@ -1,31 +1,9 @@
-package main 
+package main
 
 import "fmt"
 
-type node struct {
-	data int
-	next *node 
-}
-
-type nody interface {
-	info()
-}
-
-func (n node) nody() {
-	fmt.Println(n.data)
-	fmt.Println(n.next)
-}
-
 func main() {
-	const (
-		_ = iota
-		A 
-		B
-		C
-	)
-	 var ll = node{10, nil}
-	 ll.nody()
-	 fmt.Println(A)
-	 fmt.Println(B)
-	 fmt.Println(C)
+	slc := []int{}
+	slc = append(slc, 20, 303)
+	fmt.Println(slc, len(slc))
 }
