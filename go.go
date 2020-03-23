f7b90a39049adbd692603f7bf0c096ca61dcb2cf
theBashShell <edohbruno@gmail.com>
2020-03-23 17:13:07 +0000

    go first-class functions

--- go.go
+++ go.go
@@ -9,12 +9,21 @@ func catchpanic() {
 	}
 }
 
+func factorizor(factor, num int) func() int {
+	return func() int {
+		return num * factor
+	}
+}
+
 func main() {
 	defer catchpanic()
 	slc := []int{}
 	slc = append(slc, 20, 303)
 	fmt.Println(slc, len(slc))
-	slc[0] = 0
+	// slc[0] = 0
 	println("wow", slc)
 	if slc[0] == 0 {panic("noooo, not zero")}
+
+	f := factorizor(10, 8)
+	fmt.Println(f())
 }
