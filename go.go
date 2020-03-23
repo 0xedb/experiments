1c7ad25e30ff32b5e834f94b04f614ea51eb980c
theBashShell <edohbruno@gmail.com>
2020-03-23 17:04:50 +0000

    go recover + panic busted

--- go.go
+++ go.go
@@ -2,8 +2,19 @@ package main
 
 import "fmt"
 
+func catchpanic() {
+	r := recover()
+	if r != nil{
+	fmt.Println(r)
+	}
+}
+
 func main() {
+	defer catchpanic()
 	slc := []int{}
 	slc = append(slc, 20, 303)
 	fmt.Println(slc, len(slc))
+	slc[0] = 0
+	println("wow", slc)
+	if slc[0] == 0 {panic("noooo, not zero")}
 }
